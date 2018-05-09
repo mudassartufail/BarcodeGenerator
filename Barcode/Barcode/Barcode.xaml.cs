8fbb52ee4fefd33c5564dafdcc45eec09ffde669
mudassartufail <mudassartufail@live.com>
2018-05-09 19:12:36 +0500

    Fixed Layout

--- Barcode/Barcode/Barcode.xaml.cs
+++ Barcode/Barcode/Barcode.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace BarCodeExample
@@ -25,7 +16,7 @@ namespace BarCodeExample
             InitializeComponent();
 
             //Creating Barcode with a dummy product code. Note: You can customize accordint to your needs.
-            string productCode = "ABC1234890";
+            string productCode = "ABC123456";
             CreateBarCode(productCode);
         }
 
